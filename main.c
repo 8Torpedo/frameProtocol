ffad79c6ffcea969328cec7404403f8cb973e980
8Torpedo <ciok.blazej@gmail.com>
2016-04-27 17:35:37 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/*
-Data protocol program. Data is modified by parameters putted by user in console.
-Firs parameter is command. If command is 1, we turn camera on , if it is 0 we turn camera off .
-Second parameter is camera adress. We have 4 cameras witch are adressed : 0 , 1 , 2 , 3.
-There are four informations in one frame: command, cameraAdrr, state, checkSum.
-Without parameters program is shutting down all cameras.
-*/
-
 int main(int argc, char *argv[])
 {
 	char frame[4];
